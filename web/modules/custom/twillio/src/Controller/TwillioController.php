a0251c50e13ed95bfb9c0165749bc0472d2e726e
Ben Derenzi <benderenzi@Bens-MacBook-Pro.local>
2023-02-03 15:08:26 -0500

    implement twilio Service

--- web/modules/custom/twillio/src/Controller/TwillioController.php
+++ web/modules/custom/twillio/src/Controller/TwillioController.php
@@ -2,22 +2,32 @@
 
 namespace Drupal\twillio\Controller;
 use Drupal\Core\Controller\ControllerBase;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class TwillioController extends ControllerBase {
-    public function view($test){
-        $valid = $this->verifyNumber($test);
+    
+    public function view($phone_number){
+        $valid = $this->verifyNumber($phone_number);
 
-        dump($valid);
-        
-        
-        $data = array(
+
+        $phone_number_data = array(
             'valid' => $valid -> valid,
-            'country_code' => $valid -> countryCode
+            'countryCode' => $valid -> countryCode
           );
-          $response = new JsonResponse($data);
-          return $response;
-       
+          if($phone_number_data['valid'] == false){
+            // dump($verify_number_response_invalid_number);
+            return new Response('400');
+          }
+          elseif($phone_number_data['countryCode'] != 'US'){
+            return new Response('405');
+          }
+
+          if ($phone_number_data['valid'] == true && $phone_number_data['countryCode'] == 'US'){
+            $test = $this->verifySms($phone_number);
+          }
+
+        $response = new Response($test);
+        return $response;
             
         
     }
@@ -26,13 +36,25 @@ class TwillioController extends ControllerBase {
     public function verifyNumber($phone_number){
         $twillio_connector_service = \Drupal::Service(id: 'twillio.api_connector');
         $response = $twillio_connector_service->verifyNumber($phone_number);
-       
+
         if(!empty($response)){
             return $response;
         } else {
             return 'error';
         }
     }
+
+    public function verifySms($phone_number){
+        $twillio_connector_service = \Drupal::Service(id: 'twillio.api_connector');
+        $response = $twillio_connector_service->verifySms($phone_number);
+        // dump($response . ' in verify sms (controller)');
+        if(!empty($response)){
+            return $response;
+        } else {
+            return 'error';
+        }
+    }
+
 }
 
 ?>
